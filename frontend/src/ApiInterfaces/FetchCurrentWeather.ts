ec10edf7e1dd03f22657a56833ebae5f586a8860
Dameon Laird <dameonlair@gmail.com>
2023-05-13 12:02:45 -0700

    Use applyUnitSettings

--- frontend/src/ApiInterfaces/FetchCurrentWeather.ts
+++ frontend/src/ApiInterfaces/FetchCurrentWeather.ts
@@ -2,6 +2,7 @@ import { CurrentWeather } from "./ApiDefinitions/CurrentWeather";
 import AppConfig from "../AppConfig";
 import UnitSettings from './UnitSettings';
 import apiSettings from "./apiSettings";
+import applyUnitSettings from './applyUnitSettings';
 
 const route = "CurrentWeather";
 
@@ -9,9 +10,7 @@ export default async function FetchCurrentWeather(latitude: string, longitude: s
   const query = new URLSearchParams();
   query.set("latitude", latitude);
   query.set("longitude", longitude);
-  query.set("windspeedUnit", unitSettings.windspeedUnit);
-  query.set("temperatureUnit", unitSettings.temperatureUnit);
-  query.set("precipitationUnit", unitSettings.precipitationUnit);
+  applyUnitSettings(unitSettings, query);
   const targetUrl = `${apiSettings.endpoint}/${route}?${query}`;
   const response = await fetch(targetUrl);
   const report = (await response.json()) as CurrentWeather;
