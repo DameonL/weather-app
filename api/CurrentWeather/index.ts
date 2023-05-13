9955ab604c72d6bcf726023d83f8e31776ba0f96
Dameon Laird <dameonlair@gmail.com>
2023-05-13 13:44:09 -0700

    Make multiple fetch attempts

--- api/CurrentWeather/index.ts
+++ api/CurrentWeather/index.ts
@@ -1,29 +1,36 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 
 const endpoint = "https://api.open-meteo.com/v1/forecast";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    const latitude = req.query.latitude;
-    const longitude = req.query.longitude;
-    const temperatureUnit = req.query.temperatureUnit;
-    const windspeedUnit = req.query.windspeedUnit;
-    const precipitationUnit = req.query.precipitationUnit;
+  const latitude = req.query.latitude;
+  const longitude = req.query.longitude;
+  const temperatureUnit = req.query.temperatureUnit;
+  const windspeedUnit = req.query.windspeedUnit;
+  const precipitationUnit = req.query.precipitationUnit;
 
-    const params = new URLSearchParams();
-    params.set("latitude", latitude);
-    params.set("longitude", longitude);
-    params.set("temperature_unit", temperatureUnit);
-    params.set("windspeed_unit", windspeedUnit);
-    params.set("precipitation_unit", precipitationUnit);
-    params.set("current_weather", "true");
-    const uri = `${endpoint}?${params}`;
-    
-    const response = await fetch(uri);
-    const body = await response.json();
+  const params = new URLSearchParams();
+  params.set("latitude", latitude);
+  params.set("longitude", longitude);
+  params.set("temperature_unit", temperatureUnit);
+  params.set("windspeed_unit", windspeedUnit);
+  params.set("precipitation_unit", precipitationUnit);
+  params.set("current_weather", "true");
+  const uri = `${endpoint}?${params}`;
 
-    context.res = {
-        body
-    };
+  let response: Response;
+  let attempts = 5;
+  while (!response && attempts > 0) {
+    attempts--;
+    try {
+      response = await fetch(uri);
+      const body = await response.json();
+
+      context.res = {
+        body,
+      };
+    } catch {}
+  }
 };
 
 export default httpTrigger;
