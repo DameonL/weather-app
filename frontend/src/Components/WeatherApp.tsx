59236d2cc9d81bd02f7a53bce5478f5ae9d499d3
Dameon Laird <dameonlair@gmail.com>
2023-05-13 18:15:15 -0700

    center tabs

--- frontend/src/Components/WeatherApp.tsx
+++ frontend/src/Components/WeatherApp.tsx
@@ -25,6 +25,7 @@ export default function WeatherApp() {
       <LocationSearch setLocation={setLocation} />
       <Collapse in={location != undefined}>
         <Tabs
+          centered
           sx={{ margin: "auto" }}
           indicatorColor="secondary"
           value={activeTab}
