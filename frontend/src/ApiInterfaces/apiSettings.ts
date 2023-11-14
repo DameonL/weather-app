7007197faaaa6082667216870fb104d54eda1274
Dameon Laird <dameonlair@gmail.com>
2023-11-14 08:26:07 -0800

    Changed API to external

--- frontend/src/ApiInterfaces/apiSettings.ts
+++ frontend/src/ApiInterfaces/apiSettings.ts
@@ -1,7 +1,7 @@
 import AppConfig from "../AppConfig";
 
 const apiSettings = {
-  endpoint: AppConfig.development ? "http://localhost:7071/api" : "api"
+  endpoint: AppConfig.development ? "http://localhost:7071/api" : "https://weatherdameon.azurewebsites.net/api"
 }
 
 export default apiSettings;
