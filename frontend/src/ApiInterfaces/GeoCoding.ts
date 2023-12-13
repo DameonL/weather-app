6f029bc4316b55bd49fba224e70e363cbff4b914
Dameon Laird <dameonlair@gmail.com>
2023-12-13 11:35:01 -0800

    Sanitize state, city pairs

--- frontend/src/ApiInterfaces/GeoCoding.ts
+++ frontend/src/ApiInterfaces/GeoCoding.ts
@@ -1,19 +1,19 @@
-import { CurrentWeather } from "./ApiDefinitions/CurrentWeather";
 import { GeocodePlace } from "./ApiDefinitions/GeocodePlace";
-import AppConfig from "../AppConfig";
 import apiSettings from "./apiSettings";
 
 const route = "Geocode";
 
 export async function getCoordinates(place: string): Promise<GeocodePlace[] | GeocodePlace | undefined> {
   const query = new URLSearchParams();
+  // Remove any state information from state, city pairs
+  place = place.replace(/,.*/, "");
   query.set("search", place);
 
   const targetUrl = `${apiSettings.endpoint}/${route}?${query}`;
   const response = await fetch(targetUrl);
 
   try {
-    const report = (await response.json()) as (GeocodePlace[] | GeocodePlace);
+    const report = (await response.json()) as GeocodePlace[] | GeocodePlace;
     return report;
   } catch (error) {
     return;
@@ -26,7 +26,7 @@ export function degreesToDirection(degrees: number) {
   if (degrees === 360) {
     degrees = 0;
   }
-  
+
   const degreesPerDirection = 360 / directions.length;
   return directions[Math.ceil(degrees / degreesPerDirection)];
 }
